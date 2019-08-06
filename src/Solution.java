8c274d23471f28767714a963d567266dc64ebc10
Vladimir Belkin <vladimirB2000@mail.ru>
2019-08-06 19:32:53 +0700

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String vertical = reader.readLine();
+        int m = Integer.parseInt(vertical);
+        String horizontal = reader.readLine();
+        int n = Integer.parseInt(horizontal);
+
+        for(int a = 1; a<=n; a++)
+        {for (int b =1; b<=m; b++)
+        {System.out.print("8");}
+            System.out.println();
+    }
+
+}}
